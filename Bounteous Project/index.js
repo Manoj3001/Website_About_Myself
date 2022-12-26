04db311ecadbf70665c1b4f5c259fe26511992b4
Manoj3001 <68386427+Manoj3001@users.noreply.github.com>
2022-12-26 21:10:35 +0530

    Done

---
+++ Bounteous Project/index.js
@@ -0,0 +1,19 @@
+document.getElementById("about").onclick = function () {
+    window.location.hash = "#navigateHere";
+};
+
+document.getElementById("skills").onclick = function () {
+    window.location.hash = "#skills";
+};
+
+document.getElementById("projects").onclick = function () {
+    window.location.hash = "#projects";
+};
+
+document.getElementById("intrests").onclick = function () {
+    window.location.hash = "#intrests";
+};
+
+document.getElementById("contact_me").onclick = function () {
+    window.location.hash = "#contact_me";
+};
